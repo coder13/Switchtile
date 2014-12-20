ab58887a686559abb5ccf62744d8d22dc75cca57
coder13 <choover11@gmail.com>
2014-12-20 12:04:29 -0800

    server should be working

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var fs = require("fs"),
-	http = require("http"),
-	port = +(process.argv[2]?process.argv[2]:4000),
+	Hapi = require("hapi"),
+	serverPort = +(process.argv[2]?process.argv[2]:8000),
 	users = require('users.json');
 
 process.on('SIGINT', function() {
@@ -8,22 +8,53 @@ process.on('SIGINT', function() {
 	process.kill(0)
 });
 
-var httpServer = http.createServer(function(req, res) {
-	console.log(req.url);
-	if (req.url == '/' || req.url == '/favicon.ico')
-		return;
-	if (req.method == 'GET') {
-		res.end(JSON.stringify(users[req.url.slice(1)]));
-	} else if (req.method == 'POST') {
-		req.on('data', function(data) {
-			times = JSON.parse(data);
-			for (var n in times) {
-				users[req.url.slice(1)][n] = users[req.url.slice(1)][n].concat(times[n]);
-			}
-			console.log(times);
-			res.end();
-		});
-	}
-}).listen(port, "0.0.0.0", function() {
-	console.log("Listening at: http://localhost:" + port);
+var server = new Hapi.Server();
+server.connection({
+	host: '0.0.0.0',
+	port: serverPort,
 });
+
+server.views({
+    engines: { html: require('handlebars') },
+    path: "./",
+    isCached: false,
+})
+
+// routes
+server.route({path: "/", method: "GET",
+    handler: function(request, reply) {
+		reply.view('index');
+    }
+});
+
+server.route({path: "/users/{name}", method: "GET",
+    handler: function(request, reply) {
+		reply(JSON.stringify(getUser(request.params.name)));
+    }
+});
+
+server.start(function() {
+    console.log("Server started at ", server.info.uri);
+});
+
+
+function getUser(name) {
+	if (!users[name])
+		users[name] = {};
+	return users[name];
+}
+
+// if (req.url == '/' || req.url == '/favicon.ico')
+// 		return;
+// 	if (req.method == 'GET') {
+// 		res.end(JSON.stringify(users[req.url.slice(1)]));
+// 	} else if (req.method == 'POST') {
+// 		req.on('data', function(data) {
+// 			times = JSON.parse(data);
+// 			for (var n in times) {
+// 				users[req.url.slice(1)][n] = users[req.url.slice(1)][n].concat(times[n]);
+// 			}
+// 			console.log(times);
+// 			res.end();
+// 		});
+// 	}
