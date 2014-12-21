8b94c6900396880a87a395103f9cd97f33c54ce6
coder13 <choover11@gmail.com>
2014-12-20 17:40:49 -0800

    changes to allow push

--- server.js
+++ server.js
@@ -37,7 +37,6 @@ server.route({path: "/users/{name}", method: "POST",
 	handler: function(request, reply) {
 		var data = request.payload;
 		data.time = +data.time;
-		console.log(request.params.name, data.size, data.time);
 		addTimes(request.params.name, data.size, data.time);
 		reply(data);
 	}
@@ -58,23 +57,7 @@ function addTimes(name, size, times) {
 	user = getUser(name);
 	if (!user[size]) {
 		user[size] = [times];
-		console.log(user[size]);
 	} else {
 		user[size] = user[size].concat(times);
 	}
 }
-
-// if (req.url == '/' || req.url == '/favicon.ico')
-// 		return;
-// 	if (req.method == 'GET') {
-// 		res.end(JSON.stringify(users[req.url.slice(1)]));
-// 	} else if (req.method == 'POST') {
-// 		req.on('data', function(data) {
-// 			times = JSON.parse(data);
-// 			for (var n in times) {
-// 				users[req.url.slice(1)][n] = users[req.url.slice(1)][n].concat(times[n]);
-// 			}
-// 			console.log(times);
-// 			res.end();
-// 		});
-// 	}
